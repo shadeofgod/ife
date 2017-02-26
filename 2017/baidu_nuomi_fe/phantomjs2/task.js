6263a05d058ce4ee5687ce8fb34e94f6c5146096
zoubingwu <zoubingwu@gmail.com>
2017-02-26 23:43:22 +0800

    updated phantom2 task

--- 2017/baidu_nuomi_fe/phantomjs2/task.js
+++ 2017/baidu_nuomi_fe/phantomjs2/task.js
@@ -1,30 +1,69 @@
 function searchIt(keyword, device) {
-    var ua = require('./ua.json');
+    var uaData = require('./ua.json');
     var webPage = require('webpage');
     var page = webPage.create();
     var keyword = keyword || '',
         device = device || 'iPhone 6',
-        title,
-        link,
-        dataList = [],
         url = 'https://www.baidu.com/s?wd=' + keyword;
 
-    console.log(ua[device].ua)
-        // var settings = {
-        //         userAgent = ua[device].ua
-        //     }
-        // var page.viewportSize = {
-        //     width: ua[device].width,
-        //     height: ua[device].height
-        // }
-        //
-        // if (keyword) {
-        //     page.open(url, function(status) {
-        //         console.log(keyword);
-        //         phantom.exit();
-        //     })
-        // }
-    phantom.exit();
+    page.settings.userAgent = uaData[device].ua;
+
+    page.viewportSize = {
+        width: uaData[device].width,
+        height: uaData[device].height
+    }
+
+    if (keyword) {
+        page.open(url, function(status) {
+            if (status === 'success' && page.loadingProgress >= 100) {
+                console.log('页面打开成功，抓取数据中！')
+                var t = Date.now();
+                page.includeJs('http://cdn.bootcss.com/jquery/3.1.1/jquery.min.js',
+                    function() {
+                        var result = page.evaluate(function() {
+
+                            var dataList = [];
+
+                            $('.result').each(function() {
+                                dataList.push({
+                                    'title': $(this).find('h3')
+                                        .text(),
+                                    'link': $(this).find(
+                                        'a.c-showurl').text(),
+                                    'info': $(this).find(
+                                        '.c-abstract').text()
+                                });
+
+                            });
+
+                            return {
+                                'code': 1,
+                                'msg': '抓取成功',
+                                'dataList': dataList,
+                            }
+
+                        });
+
+                        t = Date.now() - t;
+                        result.time = t;
+                        result.device = device;
+                        result.keyword = keyword;
+
+                        console.log("抓取完成！数据如下：");
+                        console.log(JSON.stringify(result));
+                        page.render('result.jpg');
+                        phantom.exit();
+                    })
+            } else {
+                result = {
+                    'code': 0,
+                    'msg': '抓取失败，页面无法打开！'
+                }
+                console.log(JSON.stringify(result));
+                phantom.exit();
+            }
+        })
+    }
 }
 
-searchIt('phantom', 'iPad')
+searchIt('phantom', 'iPad');
