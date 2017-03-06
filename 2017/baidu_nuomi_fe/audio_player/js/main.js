638375b92ea6c31be53cd8dc4c71a3959db429c7
zoubingwu <zoubingwu@gmail.com>
2017-03-07 04:37:52 +0800

    updated

--- 2017/baidu_nuomi_fe/audio_player/js/main.js
+++ 2017/baidu_nuomi_fe/audio_player/js/main.js
@@ -1,97 +1,102 @@
 (function(window, document){
 
     window.FM = (function(){
-        let liked = false;
+        let liked = true;
         let currentSongIndex, songList;
+        let cvs, audioContext, analyser, audioSrouce, frequencyData;
 
         const DOM = {
             playBtn: document.getElementById('btn-play'),
             likeBtn: document.getElementById('btn-like'),
             nextBtn: document.getElementById('btn-next'),
-            audio: document.getElementById('audio'),
+            prevBtn: document.getElementById('btn-prev'),
+            pauseBtn: document.getElementById('btn-pause'),
+            heart: document.getElementById('b'),
+            audio: new Audio(),
+            progress: document.querySelectorAll('.progress')[0],
             progressBar: document.getElementById('progressBar'),
-            time: document.getElementsByClassName('time')[0],
+            time: document.querySelectorAll('.time')[0],
             title: document.getElementById('title'),
             author: document.getElementById('author'),
             cover: document.getElementById('cover'),
-            ifra: document.getElementById('iframe')
         };
 
-        function ajax(options){
-            let opt = options || {};
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', opt.url);
-            xhr.setRequestHeader('Accept', '*/*');
-            xhr.send(null);
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4) {
-                    let status = xhr.status;
-                    if (status >= 200 && status <= 300) {
-                        opt.success && opt.success(xhr.responseText, xhr.responseXML);
-                    } else {
-                        opt.fail && opt.fail(status);
-                    }
-                }
-            }
-        };
 
         function eventsHandler() {
+            DOM.audio.addEventListener('timeupdate', function(){
+                showTime();
+            })
             DOM.playBtn.onclick = function(e){
-                if (DOM.audio.paused) {
-                    DOM.audio.play();
-                    DOM.playBtn.firstChild.className = 'icon-2x icon-pause';
-                } else {
-                    DOM.audio.pause();
-                    DOM.playBtn.firstChild.className = 'icon-2x icon-play';
-                }
+                DOM.audio.play();
+                DOM.playBtn.style.display = 'none';
+                DOM.pauseBtn.style.display = 'inline-block';
             };
+            DOM.pauseBtn.onclick = function(){
+                DOM.audio.pause();
+                DOM.pauseBtn.style.display = 'none';
+                DOM.playBtn.style.display = 'inline-block';
+            }
             DOM.likeBtn.onclick = function(){
                 liked = !liked;
-                DOM.likeBtn.firstChild.style.color =
-                    liked
-                    ? 'rgba(212, 35, 35, 0.84)'
-                    : 'rgba(64, 64, 64, 0.86)';
+                if (!liked) {
+                    DOM.heart.style.fill = '#4A4A4A';
+                } else {
+                    DOM.heart.style.fill = '#FF2C56';
+                }
             };
             DOM.nextBtn.onclick = function(){
                 currentSongIndex++;
                 if (currentSongIndex >= songList.length) currentSongIndex = 0;
                 getMusic(songList, currentSongIndex);
+            };
+            DOM.prevBtn.onclick = function(){
+                currentSongIndex--;
+                if (currentSongIndex < 0) currentSongIndex = songList.length - 1;
+                getMusic(songList, currentSongIndex);
+            };
+            DOM.progress.onclick = function(e){
+                let x = e.clientX - DOM.progress.offsetLeft;
+                let percent = x/DOM.progress.offsetWidth*100;
+                DOM.progressBar.style.left = percent + '%';
+                DOM.audio.currentTime = parseInt(DOM.audio.duration * (percent/100));
             }
         }
 
         function showTime(){
-            let totalTime = parseInt(DOM.audio.duration);
-            let currentTime = parseInt(DOM.audio.currentTime);
-            let timeLeft = totalTime - currentTime;
-            let m = parseInt(timeLeft / 60);
-            let s = parseInt(timeLeft % 60);
-            m = m < 10 ? `0${m}` : m;
-            s = s < 10 ? `0${s}` : s;
+            if (DOM.audio.duration) {
+                let totalTime = parseInt(DOM.audio.duration);
+                let currentTime = parseInt(DOM.audio.currentTime);
+                let timeLeft = totalTime - currentTime;
+                let m = parseInt(timeLeft / 60);
+                let s = parseInt(timeLeft % 60);
+                m = m < 10 ? `0${m}` : m;
+                s = s < 10 ? `0${s}` : s;
 
-            DOM.progressBar.style.left = '-' + (timeLeft/totalTime*100)+'%';
-            DOM.time.innerHTML = `${m}:${s}`;
-
-            if (timeLeft == 0) DOM.nextBtn.click();
-            setTimeout(showTime, 100);
+                DOM.progressBar.style.left = '-' + (timeLeft/totalTime*100)+'%';
+                DOM.time.innerHTML = `${m}:${s}`;
+                if (timeLeft == 0) {
+                    currentSongIndex++;
+                    if (currentSongIndex >= songList.length) currentSongIndex = 0;
+                    getMusic(songList, currentSongIndex);
+                };
+            }
         }
 
         function getMusic(info, i) {
             currentSongIndex = i;
-            DOM.title.innerHTML = info[i].name;
             DOM.audio.src = info[i].src;
+            DOM.audio.autoplay = true;
+            DOM.title.innerHTML = info[i].name;
             DOM.author.innerHTML = info[i].author;
             DOM.cover.src = info[i].cover;
         }
 
         return {
             init: function(id){
+                songList = list;
+                getMusic(songList, 0);
 
-                $.getJSON(`http://120.24.162.247:8001/api/Music?id=${id}`, function(res){
-                    songList = res;
-                    getMusic(songList, 0);
-                    showTime();
-                    eventsHandler();
-                })
+                eventsHandler();
 
             }
         }
