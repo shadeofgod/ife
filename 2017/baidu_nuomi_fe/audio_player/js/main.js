2b2dea684068a2592f3201bfa3781213570a96fc
zoubingwu <zoubingwu@gmail.com>
2017-03-08 23:32:13 +0800

    updated

--- 2017/baidu_nuomi_fe/audio_player/js/main.js
+++ 2017/baidu_nuomi_fe/audio_player/js/main.js
@@ -59,9 +59,6 @@ let FM = (function(){
             DOM.progressBar.style.left = percent + '%';
             DOM.audio.currentTime = parseInt(DOM.audio.duration * (percent/100));
         }
-        DOM.volumeSlider.parentNode.onmouseover = function() {
-
-        }
         DOM.volumeSlider.parentNode.onclick = function(e) {
             let x = e.clientX - DOM.volumeSlider.parentNode.offsetLeft;
             if (x >= 25) {
